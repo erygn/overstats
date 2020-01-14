2e73e7423cb320acaa06e90224096ed54c9067cd
erygn <etienne.reygner@gmail.com>
2020-01-14 20:58:21 +0100

    Ajout2

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import { StyleSheet, Text, View, SafeAreaView, ScrollView, TouchableOpacity, LayoutAnimation } from 'react-native';
+import * as firebase from 'firebase'
 
 export default class HomeScreen extends React.Component {
+
+  signOut = () => {
+    firebase.auth().signOut()
+  }
 
   render() {
     LayoutAnimation.easeInEaseOut();
     return (
         <SafeAreaView style={styles.container}>
           <Text>Home</Text>
+          <TouchableOpacity style={{marginTop: 32}} onPress={this.signOut}>
+            <Text>Déconnexion</Text>
+          </TouchableOpacity>
         </SafeAreaView>
       );
   }
